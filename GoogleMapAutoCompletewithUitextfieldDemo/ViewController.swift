c7ca6de26cac624fa171f7a5e24eac43d9c976a5
satheeshkumar <satheeshkumar.g@technoduce.com>
2019-06-21 13:19:59 +0530

    Initial Commit

---
+++ GoogleMapAutoCompletewithUitextfieldDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleMapAutoCompletewithUitextfieldDemo
+//
+//  Created by Technoduce on 21/06/19.
+//  Copyright © 2019 Satheesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
